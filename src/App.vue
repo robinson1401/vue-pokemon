e63cbd6d38b8bccd68591e37281b306afcdd36ad
huuson1401@gmail.com <you@example.com>
2022-07-05 16:09:33 +0700

    mini-project about game

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,81 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <main-screen
+    v-if="statusMatch === 'default'"
+    @onStart="onHandleBeforeStart($event)"
+  />
+  <interact-screen
+    v-else-if="statusMatch === 'match'"
+    :cardsContext="settings.cardsContext"
+    @onFinish="onGetResult"
+  />
+  <result-screen
+    v-else-if="statusMatch === 'result'"
+    :timer="timer"
+    @onStartAgain="statusMatch = 'default'"
+  />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import MainScreen from "./components/MainScreen.vue";
+import InteractScreen from "./components/InteractScreen.vue";
+import ResultScreen from "./components/ResultScreen.vue";
 
+import { shuffled } from "./utils/array";
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
-  }
-}
+    MainScreen,
+    InteractScreen,
+    ResultScreen,
+  },
+  data() {
+    return {
+      settings: {
+        totalOfBlocks: 0,
+        cardsContext: [],
+        startedAt: null,
+      },
+      timer: 0,
+      statusMatch: "default",
+    };
+  },
+  methods: {
+    onHandleBeforeStart(configs) {
+      this.settings.totalOfBlocks = configs.totalOfBlocks;
+
+      const firstCards = Array.from(
+        { length: this.settings.totalOfBlocks / 2 },
+        (_, i) => i + 1
+      );
+      const secondCards = [...firstCards];
+      const cards = [...firstCards, ...secondCards];
+
+      this.settings.cardsContext = shuffled(shuffled(shuffled(shuffled(cards))));
+      this.settings.startedAt = new Date().getTime();
+
+      this.statusMatch = "match";
+    },
+
+    onGetResult() {
+      this.statusMatch = "result";
+      this.timer = new Date().getTime() - this.settings.startedAt;
+    },
+  },
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="css" scoped>
+.copyright {
+  position: fixed;
+  left: 50%;
+  transform: translateX(-50%);
+  bottom: 1.5rem;
+  color: var(--light);
+  z-index: 3;
+  font-size: 1.5rem;
+}
+
+.copyright a {
+  color: #f4dc26;
 }
 </style>
---
+++ src/components/CoppyRightScreen.vue
@@ -0,0 +1,5 @@
+<template>
+    <p>This game owned by Robin Son in Vue 3 course for begginers - 
+        <a href="#">view here</a>
+    </p>
+</template>
